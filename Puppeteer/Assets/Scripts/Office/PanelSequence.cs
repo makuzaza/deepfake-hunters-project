064ffa3baf370a36396315b1a760d3b638ff94e7
Branx <Brandon.bosquezgonzal@metropolia.fi>
2026-06-24 12:06:37 +0200

    Update PanelSequence.cs

--- Puppeteer/Assets/Scripts/Office/PanelSequence.cs
+++ Puppeteer/Assets/Scripts/Office/PanelSequence.cs
@@ -34,27 +34,27 @@ public class PanelSequence : MonoBehaviour
     {
         // 1. Título
         titleText.gameObject.SetActive(true);
-        yield return StartCoroutine(TypeText(titleText, "It's your first day at work. You just got hired at a Marketing Agency"));
+        yield return StartCoroutine(TypeText(titleText, "Welcome to your first day at Human Agency Marketing. We’re excited to have you on board."));
         yield return new WaitForSeconds(delayBetweenTexts);
 
         // 2. Línea 1
         line1Text.gameObject.SetActive(true);
-        yield return StartCoroutine(TypeText(line1Text, "Your boss seems friendly enough..."));
+        yield return StartCoroutine(TypeText(line1Text, "Our agency values creativity, adaptability, and a strong sense of responsibility."));
         yield return new WaitForSeconds(delayBetweenTexts);
 
         // 3. Línea 2
         line2Text.gameObject.SetActive(true);
-        yield return StartCoroutine(TypeText(line2Text, "Just follow instructions and you'll be fine."));
+        yield return StartCoroutine(TypeText(line2Text, "You were selected because you consistently deliver results and understand what audiences respond to"));
         yield return new WaitForSeconds(delayBetweenTexts);
 
-        // 4. Desaparecer textos anteriores
+        // 4. Desaparecer textos anteriores LIMPIA LA PANTALLA***
         titleText.gameObject.SetActive(false);
         line1Text.gameObject.SetActive(false);
         line2Text.gameObject.SetActive(false);
 
         // 5. Texto final
         finalText.gameObject.SetActive(true);
-        yield return StartCoroutine(TypeText(finalText, "Just do what your boss says and it'll be fine..."));
+        yield return StartCoroutine(TypeText(finalText, "Your ability to persuade others is exactly why the agency chose you."));
         yield return new WaitForSeconds(delayBetweenTexts);
 
         // 6. Fade out del panel
